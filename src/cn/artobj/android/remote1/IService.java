ced142b5e72684967ea3db6be33a139ec6be095a
刘洪彬 <artwebs@126.com>
2015-05-25 18:50:10 +0800

    优化json目录结构

--- src/cn/artobj/android/remote1/IService.java
+++
@@ -1,8 +0,0 @@
-package cn.artobj.android.remote1;
-
-public interface IService {
-	
-	public abstract void openConn(ServerConfig config);
-	public abstract byte[] invoke(RData data) throws ServiceException;
-	public abstract void closeConn();
-}
--- src/cn/artobj/android/remote1/IServiceCallBack.java
+++
@@ -1,5 +0,0 @@
-package cn.artobj.android.remote1;
-
-public interface IServiceCallBack {
-	public void sendMessage(RData data);
-}
